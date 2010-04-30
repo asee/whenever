7b84466cd92c858a03ecc302ae3341b1fb4ff10d
Tim Morton <tim@timothymorton.com>
2010-04-30 15:30:42 -0400

    That didn't work.  Reverting.

--- lib/whenever.rb
+++ lib/whenever.rb
@@ -4,19 +4,20 @@ require 'chronic'
 begin
   load 'Rakefile'
 rescue LoadError
-  # If Rails' rakefile was loaded than so was active_support, but
-  # if this is being used in a non-rails enviroment we need to require it.
-  # It was previously defined as a dependency of this gem, but that became
-  # problematic. See: http://github.com/javan/whenever/issues#issue/1
-  begin
-    require 'active_support/all'
-  rescue LoadError
-    warn 'To use Whenever you need the active_support gem:'
-    warn '$ gem install activesupport'
-    exit(1)
-  end
+  nil
 end
 
+# If Rails' rakefile was loaded than so was active_support, but
+# if this is being used in a non-rails enviroment we need to require it.
+# It was previously defined as a dependency of this gem, but that became
+# problematic. See: http://github.com/javan/whenever/issues#issue/1
+begin
+  require 'active_support/all'
+rescue LoadError
+  warn 'To use Whenever you need the active_support gem:'
+  warn '$ gem install activesupport'
+  exit(1)
+end
 
 # Whenever files
 require 'whenever/base'

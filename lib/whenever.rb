db017a6f69dc6f7b83ead3e741ab5589988556ee
Tim Morton <tim@timothymorton.com>
2010-05-01 21:29:36 -0400

    Updating the active support require to support earlier versions of rails.

--- lib/whenever.rb
+++ lib/whenever.rb
@@ -11,9 +11,18 @@ end
 # if this is being used in a non-rails enviroment we need to require it.
 # It was previously defined as a dependency of this gem, but that became
 # problematic. See: http://github.com/javan/whenever/issues#issue/1
+load_failed = false
 begin
-  require 'active_support/all'
+  require 'active_support/all'    
 rescue LoadError
+  # In earlier rails versions, we activate active_support like this:
+  begin
+    require 'activesupport'
+  rescue LoadError
+    load_failed = true
+  end
+end
+if load_failed
   warn 'To use Whenever you need the active_support gem:'
   warn '$ gem install activesupport'
   exit(1)
